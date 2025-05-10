fcf93e41a0052d7c3a88fdb9699c432c32ac89f3
Maolink <ethan.lackner@yahoo.com>
2025-05-10 16:56:09 -0400

    Serializers.py default values update

--- home/serializers.py
+++ home/serializers.py
@@ -5,11 +5,24 @@ class CollegeCourseSerializer(serializers.ModelSerializer):
     prerequisites = serializers.PrimaryKeyRelatedField(
         many=True, queryset=CollegeCourse.objects.all(), required=False
     )
+    description  = serializers.SerializerMethodField()
+    room = serializers.SerializerMethodField()
 
     class Meta:
         model = CollegeCourse
         fields = '__all__'
         # fields = ['name', 'course_id', 'major', 'description', 'credits', 'enrollment_requirements', 'meeting_times', 'room', 'prerequisites']
+        #incomplete
+    
+    def getDesc(self, obj):
+        return obj.description if obj.description else "No description available at this time."
+    
+    def getRoom(self, obj):
+        return obj.room if obj.room else "There is no room assigned at this time."
+    
+    def getRecs(self, obj):
+        return obj.advisorRecs if obj.advisorRecs else "Your advisor has not submitted any reccommendations about this class at this time."
+    
 
 class ScheduleSerializer(serializers.ModelSerializer):
     courses = serializers.PrimaryKeyRelatedField(
