766bd556ba894db8f65d733515dffdfdd2f6b754
Nick Benner <nbenner1@umbc.edu>
2025-05-16 09:12:25 -0400

    Added comments and made code look more readable

--- academic-pathways/src/App.tsx
+++ academic-pathways/src/App.tsx
@@ -55,55 +55,62 @@ function App() {
     events: [],
     selectedDate: '2025-05-12',
   });
-
+  // code for scrolling back to the calendar, since the calendar is at the top of the webpage
   const backToCalendar = () => {
     window.scrollTo({top: 0, behavior: 'smooth'})
   }
-
+  // updates semesterCourses based on the refresh value that is incremented every time the courseLister adds or removes a new class
   useEffect(() => {
     api.get(`schedules/${currentSchedule}/courses`).then(res => setSemesterCourses(res.data)).catch(err => console.error(err))
   }, [refresh])
 
 
   useEffect(() => {
-    if (!eventServicePlugin.eventsFacade) return;
-    console.log("hello")
+    // if there is an error with the calendar event API the function will stop as a fail-safe
+    if (!eventServicePlugin.eventsFacade) return; 
+
+    // this block of code will remove all events so that new events can be added if there is any change in the database
     eventServicePlugin.getAll().forEach(event => {
       eventServicePlugin.remove(event.id)
     })
+    // this block of code will take each course in the semesterCourses list and add it to the calendar.
     semesterCourses.forEach(course => {
+      //This part of code will parse the dictionary of class times, and create a string that will be the key for the ending meeting time.
       Object.entries(course.meeting_times).filter(([key]) => 
         key.endsWith('Start')).forEach(([startKey, start]) => {
           if (start !== "") {
+            // only creates a calendar event if there is a valid start time, if the value of the key doesn't have an empty string
             const day = startKey.replace(/Start$/, '')
             const endTime = course.meeting_times[`${day}End`]
             eventServicePlugin.add({
-              id: `${course.id}-${day}`,
+              id: `${course.id}-${day}`, // create a unique ID for the event based on the day
               title: course.course_id,
-              start:course.meeting_times[startKey],
-              end: endTime,
-              resource: { credits: Number(course.credits)},
-              description: course.description
+              start:course.meeting_times[startKey], // get start time from dictionary
+              end: endTime, // get end time from dictionary using the key made for parsing strings
+              resource: { credits: Number(course.credits)}, 
+              description: course.description // description of the class if you hover over the event
             })
-            console.log("event was created probably")
           }
         })
     })
+    // these dependencies will run the calendar updater again if there is an update to the event facade so that the check can be ran again if the value changes,
+    //and will run the calendar updater again if there is a change in the semester course list
   }, [semesterCourses, eventServicePlugin.eventsFacade])
 
+  // this function will update a refresh variable when the courseLister component notices that an add/remove button is pressed
   const handleAddedCourse = () => {
     setRefresh(k => k+1)
   }
+  // constant for credit requirements
+  const MAX_CREDITS = 18
 
-  const maxCredits = 18
+  const FULL_TIME_CREDITS = 12
 
-  const fullTime = 12
-
-  const totalCredits = semesterCourses.reduce((sum, course) => sum + Number(course.credits || 0), 0)
+  const totalCredits = semesterCourses.reduce((sum, course) => sum + Number(course.credits || 0), 0) // sums the number of credits in the current semester's courses
 
   if (!calendar) return <div>Loading calendar...</div>;
 
-
+  // code for showing the landing page, will pass the major chosen to useState variable
   if (!showPlanner) {
     return <LandingPage onSubmit={(major) => {
       setSelectedMajor(major);
@@ -113,11 +120,11 @@ function App() {
 
   return (
       <>
-      
       <div className="topBar" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0px 20px' }}>
         <h1 style={{ margin: 0, color:"#ffb71c"}}>My Academic Pathway</h1>
         <img src="https://styleguide.umbc.edu/wp-content/uploads/sites/113/2021/03/gold-retriever-on-black.jpg" style={{position:"absolute", width: "50px", height:"50px", left:"1120px"}}>
         </img>
+        {/* buttons for the top bar of the page, which is fixed to the top of the webpage at all times*/}
         <button onClick={backToCalendar} className="backToCalendar">
           Go Back to your Calendar
         </button>
@@ -125,17 +132,17 @@ function App() {
           Back to Main Page
         </button>
       </div>
-
+      {/* semester tab buttons, all styled in the same way as the other buttons*/}
       <div className="SemesterTabs">
-        <p></p>
-            {SEMESTERS.map((semester_value, i) => (
+            {SEMESTERS.map((semester_value, i) => ( // create a map of all semester constants. and create a button for each one
               <button className="semesterButtons" key={i} onClick={() => {setCurrentSchedule(i + 1) 
-              setRefresh(r => r + 1);}}
+              setRefresh(r => r + 1);}} // update refresh variable for changing which classes are shown in the calendar, and sets the currentSchedule to the schedule picked on the buttons
               style={{fontWeight: currentSchedule === i + 1 ? 'bold' : 'normal'}}>{semester_value}
             </button>
           ))}
       </div>
-      
+      {/*this is the calendar component for showing all of the classes the user is taking,
+        updated each time a user changes their class list*/}
       <div className="calendarContainer">
       {calendar ? (
         <ScheduleXCalendar calendarApp={calendar} />
@@ -143,18 +150,20 @@ function App() {
         <p>Loading calendar...</p>
       )}
       </div>
-
+      {/*Advisor recommendation bar, with course load, semester offering, and advisor recommendation information for each class in their schedule*/}
       <div className='advisorRec'> 
         <aside>
           <p>
+            {/*uses totalCredits function to sum all credits in current semesters class list, and compares that to the constants defined about credit load*/}
             <div className="creditLoadTotal" >Credit Load Total: {totalCredits}</div>
-            {totalCredits < fullTime ? <p className='warningText'> You're below the full time credit requirement! </p> : <p> You have enough credits to be a full time student. </p>}
-            {totalCredits > maxCredits ? <p className='warningText'> You might be taking too many classes. Make sure you know what you're setting yourself up for! </p> : <></>}
+            {totalCredits < FULL_TIME_CREDITS ? <p className='warningText'> You're below the full time credit requirement! </p> : <p> You have enough credits to be a full time student. </p>}
+            {totalCredits > MAX_CREDITS ? <p className='warningText'> You might be taking too many classes. Make sure you know what you're setting yourself up for! </p> : <></>}
           </p>
-          <p> </p>
+          {/* for each course in the semester course list, show when the class is offered and the advisor recommendations for each*/}
           {semesterCourses.map(course => (
           <div key={course.id} className="course-item">
             <h3>{course.course_id}: {course.name}</h3>
+            {/* string is formatted as "FallSpring" for being offered in both semesters, no need to change the string for the fall*/}
             <p><strong>Semesters Offered:</strong> {course.semester === 'FallSpring' ? 'Offered both semesters' : course.semester}</p>
             {course.advisorRecs && (
               <p><strong>Advisor Recommendations:</strong> {course.advisorRecs}</p>
@@ -163,14 +172,13 @@ function App() {
         ))}
         </aside>
       </div>
-      
-      
+      {/*course listing component, from the courseLister.tsx file. more information is in that file about this component*/}
       <div className="tableContainer">
         <h1 className="courseTableHeaderText">Pick a Course:</h1>
         <CourseLister
           major={selectedMajor}
           scheduleUsed={currentSchedule}
-          columns={courseColumns}
+          attributes={courseColumns}
           onAdded={handleAddedCourse}
         />
       </div>
--- academic-pathways/src/components/courseLister.tsx
+++ academic-pathways/src/components/courseLister.tsx
@@ -2,8 +2,6 @@ import React, {useState, useEffect, useMemo} from 'react';
 import './courseLister.css';
 import api from '../api';
 
-type Mt = Record<string,string>;
-
 interface Course {
     id: number;
     name: string;
@@ -11,58 +9,58 @@ interface Course {
     description: string;
     enrollment_requirements: string;
     prerequisites: number[];
-    meeting_times: Mt;
+    meeting_times: Record<string, string>;
     room: string;
     credits: string;
     major: string;
 }
 
 type courseListerParams = {
-    columns: string[]
+    attributes: string[]
     scheduleUsed: number
     major: string
     onAdded: () => void
 }
-
-function formatMeetingTimes(mt: Mt): string {
+  // This function will change the meeting time dictionary values into something that is more readable for the user, and display it in the course list
+function formatMeetingTimes(meetingTimes: Record<string, string>): string {
   const days = ['Monday','Tuesday','Wednesday','Thursday'];
 
+  // create a map of all meeting times in dictionary
   return days.map(day => {
-      const start = mt[`${day}Start`];
-      if (!start) return null;
-      const end = mt[`${day}End`] || '';
+      // create keys for each entry in dictionary
+      const start = meetingTimes[`${day}Start`];
+
+      if (!start) return null; // return null if there is no meeting time
+
+      const end = meetingTimes[`${day}End`] || '';
       
       const startTime = start.split(' ')[1]; 
       const endTime   = end.split  (' ')[1];
-      
-      const dayAbbrev = day.slice(0,3);
-      return `${dayAbbrev} ${startTime}–${endTime}`;
+      // abbreviate each day for printing purposes
+      const dayAbbreviation = day.slice(0,3);
+      return `${dayAbbreviation} ${startTime}–${endTime}`;
     })
-    .filter(Boolean)                         // drop any nulls
+    .filter(Boolean) // only take days that have a starting/ending time
     .join(', ');
 }
 
-export default function CourseLister({columns, scheduleUsed, major, onAdded} : courseListerParams) {
+export default function CourseLister({attributes, scheduleUsed, major, onAdded} : courseListerParams) {
     const [courses, courseSetter] = useState<Course[]>([]);
-    const [loading, loadingSetter] = useState(true);
-    const [addedToSchedule, setAddedToSchedule] = useState(1);
+    const [changedSchedule, setChangedSchedule] = useState(1); // increments when there is a change to the schedule using the buttons so that the app.tsx can update the calendar
     const [scheduleCoursesMap, setScheduleCoursesMap] = useState<{[scheduleId: number]: Course[]}>({});
-    
-    const courseMap = useMemo(() => Object.fromEntries(courses.map(c => [c.id, c])), [courses]) // creates a map with all of the courses and their ids to look up by id
+    // creates a map with all of the courses and their ids to look up by id, needed dependency to that this changes each time a new course is added so uses a useMemo
+    const courseMap = useMemo(() => Object.fromEntries(courses.map(c => [c.id, c])), [courses]) 
 
-    
-    var majorFilter = ""
+    // filter courses by major passed in by App.tsx
+  const majorFilter = major || "Computer Science"
 
-    if (major === "") {
-        majorFilter = "Computer Science"
-    } else (majorFilter = major)
-
+    // for pre requisite tracking, create an array of each semester before the semester selected
    useEffect(() => {
-    const semestersToFetch = Array.from({length: scheduleUsed}, (_, i) => i + 1)
-    semestersToFetch.forEach(id => {
+    const getPastSemesters = Array.from({length: scheduleUsed}, (_, i) => i + 1)
+    getPastSemesters.forEach(id => {
         if (scheduleCoursesMap[id]) return;
     
-
+      // get all courses from these schedules to track prerequisites
     api.get<Course[]>(`/schedules/${id}/courses/`)
          .then((res) => {
           setScheduleCoursesMap((prev) => ({
@@ -70,13 +68,15 @@ export default function CourseLister({columns, scheduleUsed, major, onAdded} : c
             [id]: res.data
           }))
          }).catch(console.error);
-    }, [scheduleUsed, addedToSchedule])
+    }, [scheduleUsed, changedSchedule]) // dependencies needed for updates on schedule and when a course is added to a schedule
   })
 
-  useEffect(() => { // Recreates the map of schedules and courses so it's reloaded when you pick any class
-  if (!scheduleUsed) return;
+  // Recreates the map of schedules and courses so it's reloaded when you pick any class
+  useEffect(() => {
+  if (!scheduleUsed) return; // failsafe if there isn't a schedule passed in
 
-  (async () => {
+    //updates courses in schedules in the current semester and courses past that 
+  const courseMapUpdater = async () => {
     const newMap: { [key: number]: Course[] } = {};
     for (let id = 1; id <= scheduleUsed; id++) {
       try {
@@ -87,46 +87,49 @@ export default function CourseLister({columns, scheduleUsed, major, onAdded} : c
       }
     }
     setScheduleCoursesMap(newMap);
-  })();
-}, [scheduleUsed, addedToSchedule]);
+  }
+  courseMapUpdater();
+}, [scheduleUsed, changedSchedule]);
 
     useEffect(() => {
         api.get('courses/').then(response => {courseSetter(response.data)}).catch(err => console.error(err));
-    }, [addedToSchedule])
+    }, [changedSchedule])
     // need to add list of schedules from the schedule buttons on the bottom of the page
     
+    //returns a boolean value if the pre-requisites of a course are met by looking at each previous semester
     function prereqsMet(course: Course): boolean {
         const priorSchedules = Object.entries(scheduleCoursesMap).filter(([key]) => Number(key) < scheduleUsed).map(([, arr]) => arr)
         const takenIds = new Set<number>(
             priorSchedules.flatMap(arr => arr.map(c => c.id))
         )
-        return course.prerequisites.every(pr => takenIds.has(pr));
+        return course.prerequisites.every(pr => takenIds.has(pr)); // return true if each prerequisite has the ids of a taken class in the past
     }
 
     const addToSchedule = (coursePrimaryKey: number): void => {
-        setAddedToSchedule(addedToSchedule + 1);
+        setChangedSchedule(changedSchedule + 1);
         api.post(`schedules/${scheduleUsed}/courses/`, {course: coursePrimaryKey}).then(() => {
             onAdded()
-            setAddedToSchedule(prev => prev + 1)
-        }) // onAdded triggers reloading of schedule
+            setChangedSchedule(prev => prev + 1)
+        }) // onAdded triggers reloading of schedule, passed back up to App.tsx
     }
     const removeFromSchedule = (coursePrimaryKey: number): void => {
         api.delete(`schedules/${scheduleUsed}/courses/`, {data: {course: coursePrimaryKey}}).then(() => onAdded() ) // onAdded triggers reloading of schedule
     }
 
     const courseList = courses.filter(c => {
-        return c.major === major
+        return c.major === majorFilter
     });
+    
 
     return (
         <table className="courseList">
             <thead>
             <tr>
-                {columns.map(col => (
+                {attributes.map(attrval => (
                     <th
-                        key = {col}
+                        key = {attrval}
                     >
-                        {col.replace(/_/g, ' ')}    
+                        {attrval.replace(/_/g, ' ')}    
                     </th>
                     
                 ))}
@@ -137,27 +140,28 @@ export default function CourseLister({columns, scheduleUsed, major, onAdded} : c
             </thead>
       <tbody>
         {courseList.map(course => {
-          const ok = prereqsMet(course);
-
+          const prereqsTaken = prereqsMet(course); // boolean variable for if the current courses prerequisites are met
+            // the prerequisites listed out in a coulmn view so that the user can see the prerequisites
             const prereqCourseIDs = course.prerequisites.length ? course.prerequisites.map(id => courseMap[id].course_id ?? `${id}`).join(', ') : 'None';
 
           return (
             <tr key={course.id}>
-              {columns.map(col => (
-                <td key={`${course.id}-${col}`}>
-                  {col === 'meeting_times'
+              {attributes.map(attrval => (
+                <td key={`${course.id}-${attrval}`}>
+                  {/* if the row of data we are on are the meeting times, return the formatted meeting times*/}
+                  {attrval === 'meeting_times'
                     ? formatMeetingTimes(course.meeting_times)
-                    : String((course as any)[col])}
+                    : String((course as any)[attrval])}
                 </td>
               ))}
               <td>{prereqCourseIDs}</td>
               <td className='buttonText'>
                 <button
                   className="addButton"
-                  disabled={!ok}
+                  disabled={!prereqsTaken}
                   onClick={() => addToSchedule(course.id)}
                 >
-                  {ok ? 'Add' : 'Missing Prereqs'}
+                  {prereqsTaken ? 'Add' : 'Missing Prereqs'}
                 </button>
               </td>
               <td className='buttonText'>
@@ -174,7 +178,4 @@ export default function CourseLister({columns, scheduleUsed, major, onAdded} : c
             </tbody>
         </table>
     );
-    // const createCourse = (courseData) => {
-    //     api.post('courses/', courseData).then(response => setCourses())
-    // }
 }
